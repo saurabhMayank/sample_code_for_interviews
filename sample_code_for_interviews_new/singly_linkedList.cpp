e46bdaa152d8a010d3377eac0f2592b20b92d330
mayank_saurabh <mayank.saurabh@elucidata.io>
2021-05-29 14:06:41 +0530

    sample code snippets to be used in interviews

---
+++ sample_code_for_interviews_new/singly_linkedList.cpp
@@ -0,0 +1,9 @@
+// Linked List Sample code to be Used in Interviews
+// For initializing class and main function
+
+// Node in a LinkList
+class Node {
+    public:
+    int data;
+    Node *next;
+};
