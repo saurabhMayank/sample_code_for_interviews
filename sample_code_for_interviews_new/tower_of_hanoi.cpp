b65b0d083049bb41fa250f92ab8f8fce0f40214a
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-02-06 18:26:00 +0530

    Update tower_of_hanoi.cpp

--- sample_code_for_interviews_new/tower_of_hanoi.cpp
+++ sample_code_for_interviews_new/tower_of_hanoi.cpp
@@ -2,6 +2,7 @@ void Hanoi(int m, string start, string middle, string end){
     cout << "m is equal to: " << m << endl;
     if(m == 1){
         cout << "Move Disc " << " from " << start << "  to " << end << endl;
+        return;
     }
     else{
         Hanoi(m-1,start,end,middle);
@@ -19,3 +20,5 @@ int main(){
 // to find that
 // also pass a count variable in the hanoi function that is initialized to 0
 // Jitne baar hanoi function call hoga utne baar count++ kar denge..
+// intialize count = 0; and whenever Hanoi function is called
+// at it on the top just do count++
