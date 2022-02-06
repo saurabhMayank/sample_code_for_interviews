370224ed2ada5fa2b1380c88f60fc679f0263f32
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-02-06 16:25:54 +0530

    Create tower_of_hanoi.cpp

---
+++ sample_code_for_interviews_new/tower_of_hanoi.cpp
@@ -0,0 +1,21 @@
+void Hanoi(int m, string start, string middle, string end){
+    cout << "m is equal to: " << m << endl;
+    if(m == 1){
+        cout << "Move Disc " << " from " << start << "  to " << end << endl;
+    }
+    else{
+        Hanoi(m-1,start,end,middle);
+        cout << "Move disc " << m << " from " << start << " to " << end << endl;
+        Hanoi(m-1,middle,start,end);
+    }
+}
+int main(){
+    int discs = 3;
+    Hanoi(discs, "start","middle","end");
+
+}
+
+// If told to calculcate no of steps then
+// to find that
+// also pass a count variable in the hanoi function that is initialized to 0
+// Jitne baar hanoi function call hoga utne baar count++ kar denge..
