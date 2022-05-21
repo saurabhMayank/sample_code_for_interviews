94cc820224131ba0569db85353c8a1318b529750
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-05-21 16:32:47 +0530

    Create print_stairs_path_with_jumps.cpp

---
+++ sample_code_for_interviews_new/recursion/print_stairs_path_with_jumps.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+
+    void printMazePaths(int sr, int sc, int dr, int dc, string psf) {
+        // write your code here
+        if(sr == dr && sc == dc){
+            cout << psf << endl;
+            return;
+        }else if(sr > dr || sc > dc){
+            return;
+        }
+        
+        
+        
+        // horizontal movement
+        // sc => src point hain
+        // uske aagese hi badhenge
+        // toh utna offset le lete hain when looping
+        // and max Step <= dc hona chahiye
+        // wo hmaara dest hain
+        // usse aage nahin badhna
+        for(int i = 1; i+sc<= dc; ++i){
+            string step = "h" + to_string(i);
+            printMazePaths(sr, sc+i,dr, dc, psf+step);
+        }
+        
+        // vertical movement
+        for(int i = 1; i+sr<= dr; ++i){
+            string step = "v" + to_string(i);
+            printMazePaths(sr+i, sc, dr, dc, psf+step);
+        }
+        
+        // diagonal movement
+        for(int i = 1; i+sc<= dc && i+sr<=dr; ++i){
+            string step = "d" + to_string(i);
+            printMazePaths(sr+i, sc+i, dr, dc, psf+step);
+        }
+        
+    }
+
+int main() {
+        int n ;
+        int m ;cin>>n>>m;
+        printMazePaths(0, 0, n - 1, m - 1, "");
+    }
