8c240f5d9691f27c402302b9c865caca859bda45
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-05-25 20:06:25 +0530

    Update 01_knapsack.cpp

--- sample_code_for_interviews_new/dynamic_programming/01_knapsack.cpp
+++ sample_code_for_interviews_new/dynamic_programming/01_knapsack.cpp
@@ -31,19 +31,23 @@ int 01_knapsack(vector<int> wts, vector<int> val, int wt_limit, int n){
     if(wts[n-1]<= wt_limit){
       //choose the option
       int output_1 = val[n-1];
-      wt_1 = wts;
-      wt_1.pop_back();
-      val_1 = val;
-      val_1,pop_back();
-      output_1 = output_1 + 01_knapsack(wt_1, val_1, wt_limit-wt[n-1], n-1);
-      int output_2 = 01_knapsack(wt_1, val_1, wt_limit, n-1);
+//       wt_1 = wts;
+//       wt_1.pop_back();
+//       val_1 = val;
+//       val_1,pop_back();
+      // pop back karne ki zaroorat nahin hain coz i am taking size, wo hmesha kam kar rhe hain
+      // toh usse wo particular element access ho jaaega
+      // original array hi pass kar do, size decrease kar do
+      // agar size nahin lia hota toh pop karwaana padta
+      output_1 = output_1 + 01_knapsack(wt, val, wt_limit-wt[n-1], n-1);
+      int output_2 = 01_knapsack(wt, val, wt_limit, n-1);
       return max(output_1, output_2);
     }else {
-      wt_1 = wts;
-      wt_1.pop_back();
-      val_1 = val;
-      val_1,pop_back();
-      int output_3 = 01_knapsack(wt_1, val_1, wt_limit, n-1);
+//       wt_1 = wts;
+//       wt_1.pop_back();
+//       val_1 = val;
+//       val_1,pop_back();
+      int output_3 = 01_knapsack(wt, val, wt_limit, n-1);
       return output_3;
     }
 }
