2be392fa6f51705ed97699c5421c1a6661a48e94
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-05-29 17:13:20 +0530

    Create 01_knapsack_dp_tabulation.cpp

---
+++ sample_code_for_interviews_new/dynamic_programming/01_knapsack_dp_tabulation.cpp
@@ -0,0 +1,31 @@
+
+// constraints 
+// n = 100
+// W = 1000
+
+
+int dp[101][1001];
+
+int knapsack_dp_tabulation(vector<int> wt, vector<int> val, int n, int W){
+  
+  for(int i = 0; i< n+1; ++i){
+    for(int j = 0; j< W+1; ++j){
+      if( i == 0 || j == 0){
+          dp[i][j] = 0;
+      }
+    }
+  }
+  
+   for(int i = 1; i < n+1; ++i){
+    for(int j = 1; j< W+1; ++j){
+       if(wt[i-1] <= W){
+         dp[i][j] = max((val[i-1] + dp[i-1][j-wt[i-1]]), dp[i-1][j]);
+       } else if(wt[i-1] > W){
+         dp[i][j] = dp[i-1][j];
+       }
+    }
+  }
+  
+  return dp[n][W];
+  
+}
