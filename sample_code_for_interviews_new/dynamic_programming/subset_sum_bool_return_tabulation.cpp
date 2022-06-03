09b3532ed6714d1a75cc76e6c6edc3f385a7db7f
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-06-03 10:19:04 +0530

    Update subset_sum_bool_return_tabulation.cpp

--- sample_code_for_interviews_new/dynamic_programming/subset_sum_bool_return_tabulation.cpp
+++ sample_code_for_interviews_new/dynamic_programming/subset_sum_bool_return_tabulation.cpp
@@ -8,29 +8,35 @@
 // given sum
 
 
-int dp[n+1][sum+1];
+  bool is_subset_sum(vector<int> arr, int sum, int n){
+        
+         int dp[n+1][sum+1]; 
+            
+          for(int i = 0;i<n+1; ++i){
+                for(int j = 0; j<sum+1; ++j){
+                    // array is empty
+                    // jab array empty and subset sum nahin mila
+                    // return false
+                  if (i == 0){
+                    dp[i][j]=  false;
+                  } else if (j == 0){
+                      // jab sum = 0 ho gya
+                      // toh iska matlab subset mil gya
+                      // jiska sum given sum ke equal hain
+                      // sum is 0
+                    dp[i][j] = true;
+                  }
+              }
+          }
 
-int subset_sum_problem_dp(int n, int sum, vector<int> arr){
-  
-  for(int i = 0;i<n+1; ++i){
-    for(int j = 0; j<sum+1; ++j){
-      if (i == 0){
-        dp[i][j]= True;
-      } else if (j == 0){
-        dp[i][j]= False;
-      }
-  }
-    
-  for(int i = 1; i<n+1; ++i){
-    for(int j = 1; j<sum+1; ++j){
-      if(arr[i-1] <= j){
-        dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
-      } else {
-        dp[i][j] = dp[i-1][j];
-      }
+          for(int i = 1; i<n+1; ++i){
+            for(int j = 1; j<sum+1; ++j){
+              if(arr[i-1] <= j){
+                dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+              }else {
+                  dp[i][j] =  dp[i-1][j];
+              }
+            }
+          }
+        return dp[n][sum];
     }
-  }
-    
-  return dp[n][sum];
-    
-}
