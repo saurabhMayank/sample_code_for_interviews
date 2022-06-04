22281c8fa2c2e82e436dc4dabfc145173d406daa
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-06-04 10:28:40 +0530

    Update count_subset_sum_with_a_given_sum_recur.cpp

--- sample_code_for_interviews_new/dynamic_programming/count_subset_sum_with_a_given_sum_recur.cpp
+++ sample_code_for_interviews_new/dynamic_programming/count_subset_sum_with_a_given_sum_recur.cpp
@@ -3,6 +3,8 @@
 // working code tested it
 
 
+
+   
 #include<vector>
 #include <bits/stdc++.h>
 using namespace std;
@@ -17,11 +19,15 @@ int count_subsets_with_a_given_sum(vector<int>arr, int sum, int n){
   
   int total_sum = 0;
   if(arr[n-1] > sum){
-
-    return count_subsets_with_a_given_sum(arr, sum, n-1);
-  } else if(arr[n-1]<= sum){
+      
+    int not_taken = count_subsets_with_a_given_sum(arr, sum, n-1);
     
-    return count_subsets_with_a_given_sum(arr, sum, n-1) + count_subsets_with_a_given_sum(arr, sum-arr[n-1], n-1);
+    return not_taken;
+  } else if(arr[n-1]<= sum){
+      
+      int taken = count_subsets_with_a_given_sum(arr, sum-arr[n-1], n-1);
+      int not_taken = count_subsets_with_a_given_sum(arr, sum, n-1);
+      return taken + not_taken; 
   }
   
 }
