57a304a2868745161145ade56451157cf430a309
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-06-19 20:53:18 +0530

    Update get_stairs_path.cpp

--- sample_code_for_interviews_new/recursion/get_stairs_path.cpp
+++ sample_code_for_interviews_new/recursion/get_stairs_path.cpp
@@ -21,7 +21,12 @@ vector<string> get_stair_paths(int n) {
   for (int step = 1; step <= 3; step++) {
     vector<string> small_ans = get_stair_paths(n - step);
     // Euler Tree mein saare steps khatam hone ke baad phir 
-    // wo tree ke post area mein aata hain jahaan se way construct hoke return hota hain
+    // Hm har recursion call se jo result aa rha hain
+    // usko store karwaate jaa rhe hain
+    
+    // tree ke uss node ke postorder area mein hmne
+    // last stored value lie uske saamne jo max step ki value hain wo jod di
+    // Phir 2nd last stored value li and uske saamne jo (max step -1) hain wo jod do
     for (int j = 0; j < small_ans.size(); j++) {
       string ss = small_ans[j];
       char no_of_steps = (char)(step + '0');
