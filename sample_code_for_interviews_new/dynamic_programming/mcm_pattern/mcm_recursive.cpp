235c6bcdef32b482efe1b3b9db18183bce4562bd
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-07-17 13:27:36 +0530

    Create mcm_recursive.cpp

---
+++ sample_code_for_interviews_new/dynamic_programming/mcm_pattern/mcm_recursive.cpp
@@ -0,0 +1,26 @@
+// mcm recursive code
+
+// input -> i and j pointers
+// arr => elements containing matrix sizes
+
+arr = [40, 20, 30, 10, 30]
+
+int i = 1;
+int j = n-1;
+int min_va
+
+int mcm_recursive(int i , int j , vector<int> arr){
+  
+  if(i>=j){
+    return 0;
+  }
+   int min = Integer.MAX_VALUE;
+  for(int k =i; k<=j-1; ++k){
+    int temp_ans = mcm_recursive(arr, i, k) + mcm_recursive(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
+    if (min > tempAns)
+        min = tempAns;
+     }
+     return min;
+  }
+}
+
