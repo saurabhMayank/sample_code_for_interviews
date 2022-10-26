5d1630f36b5c7eb711d1f767eae6528e05fb31f8
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2022-10-26 16:57:38 +0530

    Update mcm_recursive.cpp

--- sample_code_for_interviews_new/dynamic_programming/mcm_pattern/mcm_recursive.cpp
+++ sample_code_for_interviews_new/dynamic_programming/mcm_pattern/mcm_recursive.cpp
@@ -13,6 +13,18 @@ int mcm_recursive(int i , int j , vector<int> arr){
   if(i>=j){
     return 0;
   }
+   // min value is initialized here rather than globally because 
+   // if u carefully observe the recursion code
+   // Line 20 is the recursion logic
+   // So we will start from valid recursion parameters and keep going into recursion logic till
+   // base case is hit
+   // So first for mcm_recursive(arr, i, k) -> base case will hit and flow of control will come back to line number 26
+   // then for mcm_recursive(arr, k + 1, j) -> base case will hit and flow of control will come back to line number 26
+   // after that temp_ans will be calculated -> this temp ans will be compared to min and then returned
+   // now the recursive flow of control will again come back to line number 26 in one of the recursion calls
+   // So now min will not be initialized again, just its value will keep getting updated
+  
+  
    int min = Integer.MAX_VALUE;
   for(int k =i; k<=j-1; ++k){
     int temp_ans = mcm_recursive(arr, i, k) + mcm_recursive(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
