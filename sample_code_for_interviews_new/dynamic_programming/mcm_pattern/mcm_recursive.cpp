058528b24c2d377224cf2423e8d2e1726d7b8058
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2023-01-13 17:55:20 +0530

    Update mcm_recursive.cpp

--- sample_code_for_interviews_new/dynamic_programming/mcm_pattern/mcm_recursive.cpp
+++ sample_code_for_interviews_new/dynamic_programming/mcm_pattern/mcm_recursive.cpp
@@ -42,7 +42,7 @@ int mcm_recursive(int i , int j , vector<int> arr){
   
    int min = Integer.MAX_VALUE;
   for(int k =i; k<=j-1; ++k){
-    int temp_ans = mcm_recursive(arr, i, k) + mcm_recursive(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
+    int temp_ans = mcm_recursive(i, k, arr) + mcm_recursive(k + 1, j, arr) + (arr[i - 1] * arr[k] * arr[j]);
     if (min > tempAns)
         min = tempAns;
      }
