e65f5b6648d97c0e44ecf95fcafdd30aaf05fb98
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2023-01-21 20:19:30 +0530

    Update children_sum_property.cpp

--- sample_code_for_interviews_new/BinaryTrees/children_sum_property.cpp
+++ sample_code_for_interviews_new/BinaryTrees/children_sum_property.cpp
@@ -9,26 +9,26 @@ struct node {
 
 void reorder(node * root) {
   if (root == NULL) return;
-  int child = 0;
-  if (root -> left) {
-    child += root -> left -> data;
-  }
-  if (root -> right) {
-    child += root -> right -> data;
-  }
+  int child = 0; // 1
+  if (root -> left) { // 2
+    child += root -> left -> data;  // 3
+  } // 4
+  if (root -> right) { // 5
+    child += root -> right -> data; // 6
+  } // 7
 
-  if (child < root -> data) {
-    if (root -> left) root -> left -> data = root -> data;
-    else if (root -> right) root -> right -> data = root -> data;
-  }
+  if (child < root -> data) { // 8
+    if (root -> left) root -> left -> data = root -> data; // 9
+    else if (root -> right) root -> right -> data = root -> data; // 10
+  } // 11
 
-  reorder(root -> left);
-  reorder(root -> right);
+  reorder(root -> left); // 12
+  reorder(root -> right); // 13
 
-  int tot = 0;
-  if (root -> left) tot += root -> left -> data;
-  if (root -> right) tot += root -> right -> data;
-  if (root -> left || root -> right) root -> data = tot;
+  int tot = 0; // 14
+  if (root -> left) tot += root -> left -> data; // 15
+  if (root -> right) tot += root -> right -> data; // 16
+  if (root -> left || root -> right) root -> data = tot; // 17
 }
 void changeTree(node * root) {
   reorder(root);
