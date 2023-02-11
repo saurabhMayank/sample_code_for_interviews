8c0b04e921da5061bb80f7f173ae0d073f5789c8
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2023-02-11 17:18:17 +0530

    Update unbounded_knapsack_recursive.cpp

--- sample_code_for_interviews_new/dynamic_programming/unbounded_knapsack/unbounded_knapsack_recursive.cpp
+++ sample_code_for_interviews_new/dynamic_programming/unbounded_knapsack/unbounded_knapsack_recursive.cpp
@@ -1,19 +1,20 @@
+// wt array -> length of pieces array
+// W -> Rod length
+// val array => price array
 
-
-int unbounded_knapsack(vector<int> wts, vector<int> val, int wt_limit, int n){
-    if( n==0 || wt_limit == 0){
+int unbounded_knapsack(vector<int> len_arr, vector<int> price, int rod_length, int n){
+    if( n == 0 || rod_length == 0){
       return 0;
     }
-    if(wts[n-1]<= wt_limit){
+    if(len_arr[n-1]<= rod_length){
       //choose the option
-      int output_1 = val[n-1];
-      output_1 = output_1 + unbounded_knapsack(wt, val, wt_limit-wt[n-1], n);
+      int output_1 = price[n-1]+ unbounded_knapsack(len_arr, price, rod_length-len_arr[n-1], n);
       
       // not choosing the option
-      int output_2 = unbounded_knapsack(wt, val, wt_limit, n-1);
+      int output_2 = unbounded_knapsack(len_arr, price, rod_length, n-1);
       return max(output_1, output_2);
     }else {
-      int output_3 = unbounded_knapsack(wt, val, wt_limit, n-1);
+      int output_3 = unbounded_knapsack(len_arr, price, rod_length, n-1);
       return output_3;
     }
 }
