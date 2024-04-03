4369ecb60696a67a37d491948bcedef0c76da470
Mayank <82028762+saurabhMayank@users.noreply.github.com>
2024-04-03 11:42:03 +0530

    Update nearly_sorted_array.cpp

--- sample_code_for_interviews_new/binary Search/nearly_sorted_array.cpp
+++ sample_code_for_interviews_new/binary Search/nearly_sorted_array.cpp
@@ -3,24 +3,67 @@
 // Output is index of 40 in given array
 
 int searchInNearlySortedArray(vector<int> arr, int target){
-  
+  int n = arr.size();
+ 
   int low = 0;
   int high = n-1;
   int start = 0;
   int end = n-1;
+
+  if(n == 1){
+    if(arr[0] == target) {
+      return 0;
+    } else {
+      -1;
+    }
+  }
+
+  if(n == 2){
+    if(arr[0] == target){
+      return 0;
+    } else if(arr[1] == target){
+      return 1;
+    } else {
+      -1;
+    }
+  }
+
+  // better way to structure code
+  // Case 1 -> mid can be either of the middle element
+  // in that case can compare with current and both left and right element
+
+  // Case 2 -> mid can be the 0th element
+  // in that case -> left boundary already reached, compare with current and right(i+1) element
+
+  // Case 3 -> mid can be (n-1)th element
+  // in that case -> right boudary already reached, compare with current and left(i-1) element
   
   while(low<=high){
     int mid = low+(high-low)/2;
-    
-    if(arr[mid] == target){
-      return mid;
-    }else if((mid-1) > start && arr[mid-1] == target){
-      return mid;
-    }else if((mid+1) < end && arr[mid+1] == target){
-      return mid;
-    }else if(target > arr[mid]){
+
+    if((mid-1) >= 0 && (mid+1) < n){
+        if(arr[mid] == target){
+            return mid;
+        } else if(arr[mid-1] == target){
+          return mid;
+        } else if(arr[mid+1] == target){
+          return mid;
+        }
+    } else if((mid-1) < 0 && (mid+1) < n) {
+        if(arr[mid] == target){
+              return mid;
+         } else if(arr[mid+1] == target){
+          return mid;
+        }
+    } else if((mid-1) >= 0 && (mid+1) > n) {
+        if(arr[mid] == target){
+            return mid;
+        } else if(arr[mid-1] == target){
+          return mid;
+        }
+    } else if(target > arr[mid]){
       low = mid+2;
-    }else if(target < arr[mid]){
+    } else if(target < arr[mid]){
       high = mid-2;
-  }
+    }
 }
