6a200d22dc2319be14faa32e6518153be268b61d
Mayank <82028762+saurabhMayank@users.noreply.github.com>
2024-07-27 19:56:25 +0530

    Update ball_pass_game.cpp

--- sample_code_for_interviews_new/dynamic_programming/ball_pass_game.cpp
+++ sample_code_for_interviews_new/dynamic_programming/ball_pass_game.cpp
@@ -5,14 +5,46 @@ using namespace std;
 int ball_pass_helper(int& start, int curr_player,  int curr_moves, int& X, int& N) {
   
   if(curr_player == start && curr_moves > 0 && curr_moves <= X){
-    return 1;
+    
+     
+     // just doing this is not capturing all the possibilities
+     return 1;
+     
+    
+    
+    // logic not working -> core dumped coming
+    
+    // if(curr_moves == X){
+    //     return 1;
+        
+    // } else if(curr_moves <= X){
+      
+    //     // if curr_moves <= X
+       
+    //   // but this is an incomplete ans as it does not capture
+    //   // 2 -> 1 -> 2  (ans) -> but can still move
+    //   // as moves left
+    //   // 2 -> 1 -> 2 (this is not captured)
+    //   // How to capture that ?
+       
+       
+    //   // game completed and again started from same player as moves left
+       
+    //   1 + ball_pass_helper(start, curr_player, curr_moves, X, N);
+      
+    // }
+       
   }
   
   int total_ways = 0;
   
   for(int i = 1; i<=N; ++i) {
     
+
+    
     if(i != curr_player && (curr_moves <= X) && (i%curr_player == 0 || curr_player% i == 0)){
+      
+          cout << curr_player << " " << i << endl;
       
       total_ways = total_ways + ball_pass_helper(start, i, curr_moves+1, X, N);
     }
@@ -35,6 +67,10 @@ int main() {
     
 
     int ans = ball_pass_helper(start, start, curr_moves, X, N);
+    
+    cout << endl;
+    
+    cout << "---------------------------" << endl;
     
     cout << ans << endl;
 
